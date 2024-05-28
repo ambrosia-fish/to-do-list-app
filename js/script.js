956fe3651f7ca574405073e99b9cbe0f306bea58
ambrosia-fish <josefameur@gmail.com>
2024-05-28 12:27:20 -0400

    Added Delete button to li

--- js/script.js
+++ js/script.js
@@ -9,7 +9,16 @@ function newItem() {
   } else {
     $('#list').append(li);
   }
+
   //2. Crossing out an item from the list of items:
   $('li').on('dblclick', function() {
     $('li').toggleClass("strike");
   });
+
+  //3(i). Adding the delete button 'X': 
+  let crossOutButton = $('<crossOutButton>X</crossOutButton>');
+  li.append(crossOutButton);
+  $('crossOutButton').on('click', function() {
+    $('li').addClass('delete');
+  })
+
