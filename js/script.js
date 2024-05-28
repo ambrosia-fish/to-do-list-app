c6f6542d1aaa50d07b02a31bb5b4d1ad5779c433
ambrosia-fish <josefameur@gmail.com>
2024-05-28 12:27:05 -0400

    Added cross out function when dblclicking li

--- js/script.js
+++ js/script.js
@@ -9,3 +9,7 @@ function newItem() {
   } else {
     $('#list').append(li);
   }
+  //2. Crossing out an item from the list of items:
+  $('li').on('dblclick', function() {
+    $('li').toggleClass("strike");
+  });
